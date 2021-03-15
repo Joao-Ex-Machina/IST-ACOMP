5de0a03d12976fd2d6d5c0fadafb724eb03731a2
João Rodrigues <80014156+Joao-Ex-Machina@users.noreply.github.com>
2021-03-15 16:24:10 +0000

    Add files via upload

---
+++ Probs/P2/P2_Prob1c.s
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------------------------------------------+
+# Exercise 3|Calculate the second hex algarism of the constant 824								   |
+#           |                  																	   |
+#--------------------------------------------------------------------------------------------------+
+# Author: Joao Barreiros C. Rodrigues (Joao-Ex-Machina)                                            |
+# Date: 15 March 2021                                                                              |
+#-------------------------------------------------------------------------------------------------*/
+addi x10, x10, 824
+addi x11, x11, 15 #define integer register 11 as F
+
+srli x10 x10 4 #shift right the first hex algarism
+and x10 x10 x11 
+
+
